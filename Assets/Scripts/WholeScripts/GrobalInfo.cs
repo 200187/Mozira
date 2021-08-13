6619b5a7241166e667995ecaa54dd91ba10ca6a0
200187 <200187@jc-21.jp>
2021-08-13 18:33:59 +0900

    コイン、ライフ、レベルの追加

---
+++ Assets/Scripts/WholeScripts/GrobalInfo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrobalInfo : MonoBehaviour
+{
+    public static int level = 0;
+    public static int coin = 0;
+    public static int life = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
