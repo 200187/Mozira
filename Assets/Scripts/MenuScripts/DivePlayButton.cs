d5f5424a1b8c48cb1aaef9cff34971e9565a6881
育夢 佐藤 <200610@jc-21.jp>
2021-11-07 20:04:39 +0900

    DiveGameSceneに切り替えできるように修正

---
+++ Assets/Scripts/MenuScripts/DivePlayButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DivePlayButton : MonoBehaviour
+{
+    [SerializeField] private AudioSource PushSE;
+    bool isClicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clicked()
+    {
+        if (!isClicked)
+        {
+            PushSE.Play();
+            Invoke("ChangeScene", 1.0f);
+        }
+        isClicked = true;
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("DiveGameScene");
+    }
+}
+
+
+
